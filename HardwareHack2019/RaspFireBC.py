8f386b3f14e979e738dfc8e1b2c9a376c41bcdc3
Jorge Diego Rdz <jd.rdz97@gmail.com>
2019-08-24 23:00:33 -0500

    RaspFireBC changed to DummyRaspFire and probeSniffer merged with previous RaspFireBC in the file called RaspFireBC.py

--- HardwareHack2019/RaspFireBC.py
+++ HardwareHack2019/RaspFireBC.py
@@ -2,7 +2,11 @@
 import os
 import json
 import time
+from datetime import datetime
 import pyrebase
+import subprocess
+
+times = [0,0,0]
 
 # Firebase Configurations with Pyrebase
 configuration_sets = {
@@ -14,31 +18,45 @@ configuration_sets = {
     'messagingSenderId':"376675735017",
     'appId': "1:376675735017:web:5a312db977430ace"
 }
-firebase = pyrebase.initialize_app(configuration_sets)
 
-# Initialize Firebase DB
-db = firebase.database()
+def monProcess():
+    # Enable monitor mode 
+    subprocess.run(['airmon-ng', 'start', 'wlan0'])
 
-def dummy_firebase_update():
+    for x in range(3):
+        stdout = subprocess.run(['tshark', '-i', 'wlan0mon', '-f', 'wlan type mgt && subtype probe-req', '-T', 'fields', '-E', 'separator=/s', '-e', 'wlan.sa', '-a', 'duration:10'], capture_output=True, text=True).stdout
+        splitblah = stdout.split("\n")
+        dictofString=dict.fromkeys(splitblah)
+        del dictofString[""]
+        
+        times[x] = len(dictofString)
+        #time.sleep(1)
+    
+    print("times: ", times, "\n")
+    firebase_update(times)
 
+
+def firebase_update(times):
+    #disable monitor mode
+    subprocess.run(['airmon-ng', 'stop', 'wlan0mon'])
+    #restart network-manager
+    subprocess.run(['systemctl', 'restart', 'network-manager'])
+    time.sleep(10)
+
+    firebase = pyrebase.initialize_app(configuration_sets)
+
+    # Initialize Firebase DB
+    db = firebase.database()
+    #upProcess()
+    
     # Get the actual time of update
     current_time = str(time.time()).replace('.','')
     # Define the update input for the FireBase RT_DB
-    crowd_size_update = {str(current_time):12}
+    crowd_size_update = {str(current_time):times}
     # Get the push done
     app_json = json.dumps(crowd_size_update)
-    db.child("Places").child("Biblioteca_1").push(crowd_size_update)
+    db.child("Places").child("Centro_Estudiantil").push(crowd_size_update)
 
-
-# Dummy variable for testing
-counter = 0
-
-# Updating loop for Ops
-while(counter < 5):
-    # Call Firebase Update Function
-    dummy_firebase_update()
-    # Update Counter Variable
-    counter += 1
-    # Rest process during 20 units of time
-    time.sleep(10)
-
+while(1):
+    monProcess()
+    #time.sleep(1)
